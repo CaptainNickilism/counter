b164d5636387fa8427bb6cb6787dff307c23a1dd
CaptainNickilism <82886692+CaptainNickilism@users.noreply.github.com>
2022-04-03 23:19:23 +0200

    Add files via upload

--- js/counter.js
+++ js/counter.js
@@ -1,40 +1,43 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-   const increaseButton = document.querySelector('#decreaseButton');
-   const decreaseButton = document.querySelector('#increaseButton');
+   // DOM SELECTIONS
+   const increaseButton = document.querySelector('#decreaseButton'); // DECREASE DOM ELEMENT
+   const decreaseButton = document.querySelector('#increaseButton'); // INCREASE DOM ELEMENT
+   const counter = document.querySelector('#counter'); // COUNTER DOM ELEMENT
+   let counterNumber = Number(counter.innerHTML);      // COUNTER NUMBER
 
-   increaseButton.addEventListener('click', decreaseCounter);
-   decreaseButton.addEventListener('click', increaseCounter);
-   increaseButton.addEventListener('mousemove', shineEffect);
-   decreaseButton.addEventListener('mousemove', shineEffect);
+   // EVENT LISTENERS
+   increaseButton.addEventListener('click', decreaseCounter);  // CLICK FUNCTIONS
+   decreaseButton.addEventListener('click', increaseCounter);  // CLICK FUNCTIONS
+   increaseButton.addEventListener('mousemove', shineEffect);  // HOVER FUNCTIONS
+   decreaseButton.addEventListener('mousemove', shineEffect);  // HOVER FUNCTIONS
 
-   const counter = document.querySelector('#counter');
-   let counterNumber = Number(counter.innerHTML);
-
-   function decreaseCounter(e) {
-      counter.innerHTML = --counterNumber;
-      pressAnimation(this);
+   // COUNTER FUNCTIONS
+   function decreaseCounter(e) {                      // DECREASE
+      counter.innerHTML = --counterNumber;            // Decrease the number by 1
+      pressAnimation(this);                           // Play animation on button
    }
 
-   function increaseCounter() {
-      counter.innerHTML = ++counterNumber;
-      pressAnimation(this);
+   function increaseCounter() {                       // INCREASE
+      counter.innerHTML = ++counterNumber;            // Increase the number by 1
+      pressAnimation(this);                           // Play animation on button
    }
 
-   function shineEffect(e) {
-      let { x, y } = this.getBoundingClientRect();
-      this.style.setProperty("--x", e.clientX - x);
-      this.style.setProperty("--y", e.clientY - y);
+   // VISUAL EFFECTS
+   function shineEffect(e) {                          // HOVER "GLASS REFLECTION" EFFECT
+      let { x, y } = this.getBoundingClientRect();    // Get button size & position
+      this.style.setProperty("--x", e.clientX - x);   // Update --x CSS variable (i.e. move the reflection horizontally)
+      this.style.setProperty("--y", e.clientY - y);   // Update --y CSS variable (i.e. move the reflection vertically)
    }
 
-   function pressAnimation(button) {
-      button.classList.add('pressedButton');
-      button.ontransitionend = () => {
-         button.classList.remove('pressedButton')
+   function pressAnimation(button) {                  // CLICK VISUAL FEEDBKACK
+      button.classList.add('pressedButton');          // Add class to style button pseudo-elements
+      button.ontransitionend = () => {                // When CSS transition ends...
+         button.classList.remove('pressedButton')     // ...remove the class
       }
-      counter.classList.add('changedCounter');
-      counter.onanimationend = () => {
-         counter.classList.remove('changedCounter');
+      counter.classList.add('changedCounter');        // Add class to play fade animation
+      counter.onanimationend = () => {                // When CSS animation ends...
+         counter.classList.remove('changedCounter');  // ...remove the class
       }
    }
 
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Counter
+This is a simple counter made as a project for [Start2Impact](https://www.start2impact.it/) with HTML, CSS and JavaScript which lets the user either increase or decrease the number shown by clicking on the respective button.
+![Gif demonstrating counter usage](https://github.com/CaptainNickilism/counter/blob/47b2ef9d137aa67748778f5a42432ba4f4fd3a5e/counter_demo.gif)
+
+## Installation
+- Download the project files
+- Open "index.html" in a browser
+- Enjoy :)
+
+You can also use the counter without downloading the files: you can find an online working version at [my personal website](http://nickilism.42web.io/projects/counter/index.html)
+
+## Usage
+You can adjust the counter value by clicking either the DECREASE BUTTON ("-"), which will subtract 1, or the INCREASE BUTTON ("+"), which will add 1.
+
+## Support
+If you have any issues or suggestions, please feel free to reach out to my GitHub, [CaptainNickilism](https://github.com/CaptainNickilism).
+
+## Author
+The counter was made by me, Niccolò Pagnacco.
+
+## License
+### [MIT License](https://github.com/CaptainNickilism/counter/blob/main/LICENSE)
