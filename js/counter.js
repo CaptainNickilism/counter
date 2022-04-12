796757a3e8a7c3e81f1aca81c93044096eb310cb
CaptainNickilism <82886692+CaptainNickilism@users.noreply.github.com>
2022-04-12 15:06:09 +0200

    Mk. II
    - Removed elements from HTML (empty document body)
    - Added JavaScript code to create and manage elements dynamically

--- js/counter.js
+++ js/counter.js
@@ -1,37 +1,38 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-   let display = document.createElement('div');
-   display.id = 'display';
-   document.body.append(display);
+   // CREATE ELEMENTS
+   let display = document.createElement('div');                // Create DISPLAY CONTAINER (div)
+      let title = document.createElement('h2');                   // Create TITLE (H2)
+      let counter = document.createElement('h1');                 // Create COUNTER (H1)
+   let buttons = document.createElement('div');                // Create BUTTONS CONTAINER (div)
+      let decreaseButton = document.createElement('button');      // Create DECREASE (button)
+      let increaseButton = document.createElement('button');      // Create INCREASE (button)
 
-   let title = document.createElement('h2');
-   title.id = 'title';
-   title.innerHTML = "PRESS <strong>-</strong> OR <strong>+</strong> TO <strong>DECREASE</strong> OR <strong>INCREASE</strong> THE NUMBER.";
-   display.append(title);
+   // ASSIGN CSS SELECTORS
+   display.id = 'display';                                     // Assign CSS selector '#display'
+      title.id = 'title';                                         // Assign CSS selector '#title'
+      counter.id = "counter";                                     // Assign CSS selector '#counter'
+   buttons.id = 'buttons';                                     // Assign CSS selector '#buttons'
+      decreaseButton.id = "decreaseButton";                       // Assign CSS selector '#decreaseButton'
+      decreaseButton.classList.add('button');                     // Assign CSS selector '.button'
+      increaseButton.id = "increaseButton";                       // Assign CSS selector '#increaseButton'
+      increaseButton.classList.add('button');                     // Assign CSS class '.button'
 
-   let counter = document.createElement('h1');
-   counter.id = "counter";
-   counter.innerHTML = 0;
-   display.append(counter);
+   // FILL ELEMENTS INNER HTML
+   title.innerHTML = "PRESS <strong>-</strong> OR \
+   <strong>+</strong> TO <strong>DECREASE</strong>\
+   OR <strong>INCREASE</strong> THE NUMBER.";                 // Insert TITLE html content
+   counter.innerHTML = 0;                                      // Insert COUNTER html content
+   decreaseButton.innerHTML = "-";                             // Insert DECREASE html content
+   increaseButton.innerHTML = "+";                             // Insert INCREASE html content
 
-   let buttons = document.createElement('div');
-   buttons.id = 'buttons';
-   document.body.append(buttons);
-
-   let decreaseButton = document.createElement('button');
-   decreaseButton.id = "decreaseButton";
-   decreaseButton.classList.add('button');
-   decreaseButton.innerHTML = "-";
-   buttons.append(decreaseButton);
-
-   let increaseButton = document.createElement('button');
-   increaseButton.id = "increaseButton";
-   increaseButton.classList.add('button');
-   increaseButton.innerHTML = "+";
-   buttons.append(increaseButton);
-
-
-   let counterNumber = Number(counter.innerHTML);                    // COUNTER NUMBER
+   // DISPLAY ELEMENTS
+   document.body.append(display);                              // Append DISPLAY CONTAINER into DOCUMENT BODY
+      display.append(title);                                      // Append TITLE into DISPLAY CONTAINER
+      display.append(counter);                                    // Append COUNTER into DISPLAY CONTAINER
+   document.body.append(buttons);                              // Append BUTTONS CONTAINER into DOCUMENT BODY
+      buttons.append(decreaseButton);                             // Append DECREASE into BUTTONS CONTAINER
+      buttons.append(increaseButton);                             // Append INCREASE into BUTTONS CONTAINER
 
    // EVENT LISTENERS
    increaseButton.addEventListener('click', increaseCounter);  // CLICK FUNCTIONS
@@ -39,7 +40,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
    increaseButton.addEventListener('mousemove', shineEffect);  // HOVER FUNCTIONS
    decreaseButton.addEventListener('mousemove', shineEffect);  // HOVER FUNCTIONS
 
+
+
+
    // COUNTER FUNCTIONS
+   let counterNumber = Number(counter.innerHTML);            // READ COUNTER AS NUMBER
+
    function decreaseCounter(e) {                      // DECREASE
       counter.innerHTML = --counterNumber;            // Decrease the number by 1
       pressAnimation(this);                           // Play animation on button
